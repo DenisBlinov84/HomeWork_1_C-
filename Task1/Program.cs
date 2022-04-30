3470097767022a6a4ffdbad4f6f3590932d09fb8
DENIS BLINOV <blinovda1@mail.ru>
2022-04-30 23:14:29 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+
+string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if(a>b){
+    Console.WriteLine ("{0} наибольшее число" , a );
+    Console.WriteLine ("{0} наименьшее число" , b );
+}
+else{
+    Console.WriteLine ("{0} наибольшее число" , b );
+    Console.WriteLine ("{0} наименьшее число" , a );
+}
