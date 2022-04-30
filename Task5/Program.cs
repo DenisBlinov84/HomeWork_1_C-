6f522742b07e091b79ada38db0318daac504b516
DENIS BLINOV <blinovda1@mail.ru>
2022-05-01 01:47:21 +0300

    add task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу вычисления модуля числа.
+
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+int x;
+if (a < 0)
+{
+    x = -a;
+    Console.WriteLine(x);
+}
+else
+{
+    x = a;
+    Console.WriteLine(x);
+}
