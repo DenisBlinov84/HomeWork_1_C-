68fa1a31dc2dfe917902276f4089203d4eda0265
DENIS BLINOV <blinovda1@mail.ru>
2022-05-15 10:16:50 +0300

    fixed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, 
 // является ли число чётным (делится ли оно на два без остатка).
 
-string s_a = Console.ReadLine();
-int a = int.Parse(s_a);
+string s_n_1 = Console.ReadLine();
+int n_1 = int.Parse(s_n_1);
 
-if (a % 2 == 0)
+if (n_1 % 2 == 0)
 {
-    Console.WriteLine("Число чётное");
+    Console.Write($"The number {n_1} is even ");
 }
 else
 {
-    Console.WriteLine("Число не чётное");
+    Console.Write($"The number {n_1} is odd ");
 }
